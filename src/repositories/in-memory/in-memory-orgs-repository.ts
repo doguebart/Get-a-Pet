226f44849b81e4f531fa291f91a1cc38cbe53136
doguebart <douglasweelber.2004@outlook.com>
2024-07-11 21:09:08 -0300

    feat: Create in-memory orgs repository

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,37 @@
+import { Prisma, Org } from "@prisma/client";
+import { randomUUID } from "node:crypto";
+import { IOrgRepository } from "../orgs-repository";
+
+export class InMemoryOrgsRepository implements IOrgRepository {
+  public items: Org[] = [];
+
+  async findByEmail(email: string) {
+    const org = this.items.find((item) => item.email === email);
+
+    if (!org) {
+      return null;
+    }
+
+    return org;
+  }
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      description: data.description,
+      phone: data.phone,
+      email: data.email,
+      password_hash: data.password_hash,
+      zip_code: data.zip_code,
+      state: data.state,
+      city: data.city,
+      address: data.address,
+      created_at: new Date(),
+    };
+
+    this.items.push(org);
+
+    return org;
+  }
+}
