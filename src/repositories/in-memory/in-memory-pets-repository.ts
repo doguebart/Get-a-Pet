091510d000b6d10f3ef8ea4476c2c5611a54baab
doguebart <douglasweelber.2004@outlook.com>
2024-07-14 20:44:38 -0300

    feat: Add Pet Create method in pet repository

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,27 @@
+import { Prisma, Pet } from "@prisma/client";
+import { IPetRepository } from "../pets-repository";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryPetsRepository implements IPetRepository {
+  public items: Pet[] = [];
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = {
+      id: randomUUID(),
+      orgId: data.orgId ?? null, // Ensure orgId is not undefined
+      name: data.name,
+      specie: data.specie,
+      size: data.size,
+      age: data.age,
+      characteristics: Array.isArray(data.characteristics) // Ensure characteristics is an array of strings
+        ? data.characteristics
+        : [],
+      adopted_at: data.adopted_at ? new Date(data.adopted_at) : null,
+      created_at: new Date(),
+    };
+
+    this.items.push(pet);
+
+    return pet;
+  }
+}
