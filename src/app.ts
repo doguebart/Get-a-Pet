6d51130b1ef7c67a09c0592825328d110782b381
doguebart <douglasweelber.2004@outlook.com>
2024-07-17 15:00:09 -0300

    feat: Add JWT authentication

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { orgsRoutes } from "./http/controllers/org/routes";
 import { petRoutes } from "./http/controllers/pet/routes";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "3d",
+  },
+});
+
+app.register(fastifyCookie);
 
 app.register(orgsRoutes);
 app.register(petRoutes);
